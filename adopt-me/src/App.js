401e8a665491100dbece31b9accbd25da9c93c38
dinithchanuka <dinithchanuka22@gmail.com>
2019-10-04 14:58:09 +0530

    Intial adopt-me

---
+++ adopt-me/src/App.js
@@ -0,0 +1,37 @@
+ const Pet = ({name,animal,breed}) => {
+    return React.createElement("div",{},[
+        React.createElement("h1",{},name),
+        React.createElement("h2",{},animal),
+        React.createElement("h3",{},breed),
+    ])
+ }
+
+ const App = () => {
+    return React.createElement(
+    "div",
+    {id:"something"},
+    [
+        React.createElement("h1", {}, "Adopt Me!"),
+        React.createElement(Pet,{
+            name:"Luna",
+            animal:"Dog",
+            breed:"Havanese"
+        }),
+        React.createElement(Pet,{
+            name:"Ishi",
+            animal:"Cat",
+            breed:"Type cat"
+        }),
+        React.createElement(Pet,{
+            name:"Raka",
+            animal:"Bull",
+            breed:"Red bull"
+        })
+    ]
+    );
+};
+
+ReactDOM.render(
+    React.createElement(App),
+    document.getElementById("root")
+    );
